316e483c35386bd037e540f640a7a34a19e808ad
Kaique <skaique833@gmail.com>
2020-12-06 20:13:26 -0200

    deletando arquivos desnecessarios

--- rodape.php
+++
@@ -1,5 +0,0 @@
-<footer id="rodape" class="container-fluid bg-danger text-white">
-           <h4 id="formasPagamento"> Formas de pagamento</h4>
-           <img width="35%" src="imagens/pag.jpg" alt="Formas de pagamento">
-           <p> &copy Recode Pro</p>
-</footer>
